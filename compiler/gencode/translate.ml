866115b6523f06a8762855866372360e4480070f
TigistShiferaw <tgg909441@gmail.com>
2021-07-15 14:23:01 +0300

    commands for control_robot keywod in code generation

--- compiler/gencode/translate.ml
+++ compiler/gencode/translate.ml
@@ -476,6 +476,11 @@ let rec exp env loop_path code { Zelus.e_desc = desc } =
      print_endline("Translate");
      let e, code = exp env loop_path code e in
      Omove(e), code
+  (*added here*)
+  | Zelus.Eop(Zelus.Econtrol, [e]) ->
+     print_endline("Translate");
+     let e, code = exp env loop_path code e in
+     Ocontrol(e), code
   | Zelus.Eop(Zelus.Ehorizon, [e]) ->
      exp env loop_path code e
   | Zelus.Eop(Zelus.Eifthenelse, [e1; e2; e3]) ->
